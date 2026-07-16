c2a53219189af4a0a8358acea17679b3398683a5
Your Name <you@email.com>
2026-07-16 17:22:55 +0800

    Fix Klook affiliate URL to correct redirect link

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -8,7 +8,7 @@ import styles from './Plans.module.css';
 const AFFILIATES = [
   { name: 'Tiqets',      url: 'https://www.tiqets.com' },
   { name: 'Booking.com', url: 'https://www.booking.com' },
-  { name: 'Klook',       url: 'https://www.klook.com/?aid=127608' },
+  { name: 'Klook',       url: 'https://affiliate.klook.com/redirect?aid=127608&aff_adid=1341474&k_site=https%3A%2F%2Fwww.klook.com%2F' },
   { name: 'Expedia',     url: 'https://www.expedia.com' },
 ];
 
